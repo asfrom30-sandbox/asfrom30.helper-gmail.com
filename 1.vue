ebd4360c7ac318ac5fef26074995c15d2483afbd
Doyoon Kim <asfrom30@gmail.com>
2020-04-05 15:31:28 +0900

    initial commit

---
+++ 1.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="debug container">
+      <div class="child">child</div>
+      <div class="child">child</div>
+      <div class="child">child</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.debug {
+  border: 1px solid black;
+}
+.debug > div {
+  border: 1px solid red;
+}
+
+.container {
+  display: flex;
+}
+
+.child {
+  display: flex;
+  flex: 1 1 0;
+}
+</style>
---
+++ 2-1.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="debug container">
+      <div class="child">
+        <span class="content">child</span>
+      </div>
+      <div class="child">
+        <span class="content">child</span>
+      </div>
+      <div class="child">
+        <span class="content">child</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.debug {
+  border: 1px solid black;
+}
+.debug > div {
+  border: 1px solid red;
+}
+
+.container {
+  display: flex;
+  height: 200px;
+  align-items: center;
+}
+
+.child {
+  display: flex;
+  flex: 1 1 0;
+  height: 100%;
+}
+
+.content {
+  border: 1px solid green;
+  margin: auto;
+  /* justify-self: center; */
+  align-self: center;
+}
+</style>
